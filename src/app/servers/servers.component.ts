866fa082d46374374547893f7fdaf667e87b734a
Christoffer P <slim@ChristoffersMBP.lan>
2017-05-03 20:58:00 +0200

    Section 2 Lesson 24, before assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Testserver';  
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
-  constructor() { }
+  constructor() {
+   setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   };
 
   ngOnInit() {
+  }
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created!';
+  }
+  onHoverServer(){
+    this.serverCreationStatus = 'Dont forget!';
+  }
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
